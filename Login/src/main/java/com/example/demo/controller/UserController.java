1c8f25d4f449e1c9c1cd7e2b517ee9cab793f94d
hugo <hugo.delacruz@tecsup.edu.pe>
2019-10-31 11:26:54 -0500

    Craacion de repositorio de Usuario - roles y usuarios

--- Login/src/main/java/com/example/demo/controller/UserController.java
+++ Login/src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepoitory;
+import com.example.demo.service.UserService;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	RoleRepoitory roleRepository;
 	
 	@GetMapping("/")
 	public String index(){
 		return "index";
 	}
 	@GetMapping("/userForm")
-		public String userForm(){
+		public String userForm(Model model){
+		model.addAttribute("userForm", new User());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("listTab","active");
 			return "user-form/user-view";
 		}
 
